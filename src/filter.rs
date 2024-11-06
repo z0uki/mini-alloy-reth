c4d1e782d0e21b7f029428dcf2cba9561e26d89b
z0uki <3070700787@qq.com>
2024-11-06 15:55:48 +0800

    test

--- src/filter.rs
+++ src/filter.rs
@@ -7,7 +7,7 @@ use alloy::{
 use eyre::{OptionExt, Result};
 use reth_chainspec::ChainInfo;
 use reth_provider::{
-    BlockHashReader, BlockIdReader, BlockNumReader, HeaderProvider, ReceiptProvider,
+    BlockHashReader, BlockNumReader, BlockReader, HeaderProvider, ReceiptProvider,
 };
 use reth_rpc_eth_types::logs_utils::{self, append_matching_block_logs, ProviderOrBlock};
 
@@ -68,19 +68,29 @@ impl<P, T> RethDbProvider<P, T> {
         // // loop over the range of new blocks and check logs if the filter matches the log's bloom
         // // filter
         for (from, to) in BlockRangeInclusiveIter::new(from_block..=to_block, 1000) {
-            let headers = provider.headers_range(from..=to)?;
+            // let headers = provider.headers_range(from..=to)?;
 
-            for (idx, header) in headers.iter().enumerate() {
+            for idx in from..=to {
+                let block = provider
+                    .block_by_number(idx)?
+                    .ok_or_else(|| eyre::eyre!("block not found"))?;
+
+                let header = block.header;
+
                 // only if filter matches
                 if FilteredParams::matches_address(header.logs_bloom, &address_filter)
                     && FilteredParams::matches_topics(header.logs_bloom, &topics_filter)
                 {
-                    let block_hash = match headers.get(idx + 1) {
-                        Some(parent) => parent.parent_hash,
-                        None => provider
-                            .block_hash(header.number)?
-                            .ok_or_eyre("header not found")?,
-                    };
+                    // let block_hash = match headers.get(block + 1) {
+                    //     Some(parent) => parent.parent_hash,
+                    //     None => provider
+                    //         .block_hash(header.number)?
+                    //         .ok_or_eyre("header not found")?,
+                    // };
+                    //
+                    let block_hash = provider
+                        .block_hash(header.number)?
+                        .ok_or_eyre("header not found")?;
 
                     let num_hash = BlockNumHash::new(header.number, block_hash);
 
@@ -100,6 +110,37 @@ impl<P, T> RethDbProvider<P, T> {
                     }
                 }
             }
+
+            // for (idx, header) in headers.iter().enumerate() {
+            //     // only if filter matches
+            //     if FilteredParams::matches_address(header.logs_bloom, &address_filter)
+            //         && FilteredParams::matches_topics(header.logs_bloom, &topics_filter)
+            //     {
+            //         let block_hash = match headers.get(idx + 1) {
+            //             Some(parent) => parent.parent_hash,
+            //             None => provider
+            //                 .block_hash(header.number)?
+            //                 .ok_or_eyre("header not found")?,
+            //         };
+
+            //         let num_hash = BlockNumHash::new(header.number, block_hash);
+
+            //         if let Some(receipts) = provider
+            //             .receipts_by_block(num_hash.hash.into())
+            //             .map_err(|_| eyre::eyre!("failed to get receipts for block"))?
+            //         {
+            //             append_matching_block_logs(
+            //                 &mut all_logs,
+            //                 ProviderOrBlock::Provider(&provider),
+            //                 &filter_params,
+            //                 num_hash,
+            //                 &receipts,
+            //                 false,
+            //                 header.timestamp,
+            //             )?;
+            //         }
+            //     }
+            // }
         }
 
         drop(provider);
